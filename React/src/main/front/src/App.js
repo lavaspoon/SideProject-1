e26328739c0531840ab31f61c89178d1486eb6bd
spoonlava <lavaspoon@naver.com>
2023-08-27 01:21:02 +0900

    Test Conn

--- React/src/main/front/src/App.js
+++ React/src/main/front/src/App.js
@@ -1,25 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react'
+import axios from 'axios';
+
+function selectData(){
+    axios.post('/testData',["가","나","다"])
+        .then(function (res){
+            console.log(res)
+        });
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <div>
+                    <button onClick={() =>selectData()}>Test</button>
+                </div>
+            </header>
+        </div>
+    );
 }
 
 export default App;
